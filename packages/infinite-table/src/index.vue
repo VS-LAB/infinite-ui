dbe4eb8a063b67aca8c73f1406fd869701929616
WeiZe <501816423@qq.com>
2020-09-26 00:30:05 +0800

    feat:本地出现引用的lelment的组件中存在render函数时候编译错误的复现代码

--- packages/infinite-table/src/index.vue
+++ packages/infinite-table/src/index.vue
@@ -1,23 +1,47 @@
 <template>
-  <!-- <el-table class="infinite-table">
-
-  </el-table> -->
-  <div>
-    infinite-table
+  <div class="infinite-table">
+    <el-table :data="data"
+              width="100%">
+      <el-table-column v-for="hItem in header"
+                       :key="hItem.prop"
+                       :prop="hItem.prop"
+                       :label="hItem.label"
+                       :width="hItem.width || ''">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
-// import ElTable from 'element-ui/packages/table'
+import ElTable from 'element-ui/packages/table'
+import ElTableColumn from 'element-ui/packages/table-column'
 export default {
   name: 'InfiniteTable',
   components: {
-    // ElTable
+    ElTable,
+    ElTableColumn
   },
   props: {
+    data: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+
+    header: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+
+    width: [String, Number],
+
+    height: [String, Number],
+
+    maxHeight: [String, Number]
   },
-  methods: {
-  },
-  mounted () {
-  }
+  methods: {},
+  mounted () { }
 }
 </script>
