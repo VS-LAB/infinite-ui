beb14b4b07281927716bde9f002924ddd78ffca1
Sun <1368055622@qq.com>
2020-11-05 16:04:58 +0800

    单元测试调整

--- tests/unit/InfiniteSelectTags.spec.js
+++ tests/unit/InfiniteSelectTags.spec.js
@@ -1,28 +1,38 @@
 import { shallowMount } from '@vue/test-utils'
 import InfiniteSelectTags from '@/packages/infinite-select-tags/src/index.vue'
+// import ElInput from 'element-ui/lib/input'
+// import ElTag from 'element-ui/lib/tag'
+// import ElSelect from 'element-ui/lib/select'
+// import ElOption from 'element-ui/lib/option'
+import ElCheckbox from 'element-ui/lib/checkbox'
+// import InfiniteButton from '../../infinite-button'
 
 describe('InfiniteSelectTags.vue', () => {
   // console.log(InfiniteSelectTags)
   const wrapper = shallowMount(InfiniteSelectTags, {
-    propsData: {
-      data: { id: 'GBP',
-        name: '英镑',
-        disabled: true }
-    }
+    stubs: ['infinite-button']
   })
-  // it('click select', () => {
-  //   // const buttonWrapper = wrapper.find({ ref: 'myButton' })
-  //   wrapper.trigger('click')
-  // })
 
-  it('应该渲染正确的options的长度', () => {
-    console.log(wrapper.html())
+  // console.log(wrapper.vm)
+  console.log(wrapper.html())
 
-    // el-select
-    wrapper.trigger('focus')
-    const box = wrapper.find('.infinite-select-group')
-    // expect(box.findAll('div').length).toBe(1)
-    // expect(box.findAll('div').at(0).text().toBe('英镑'))
-    console.log(box.html())
+  it('是否正确渲染', () => {
+    expect(wrapper.html()).toContain('<div class="infinite-select-group"></div>')
+  })
+
+  // 检查已存在的元素
+  it('是否有按钮', () => {
+    expect(wrapper.contains('infinite-button-stub')).toBe(true)
+  })
+
+  it('触发一个自定义事件', () => {
+    expect(wrapper.findComponent(ElCheckbox).vm.$emit('change'))
+  })
+
+  it('触发按钮', () => {
+    // 获取对应按钮
+    const primaryButton = wrapper.find('.infinite-select-button infinite-button-stub')
+    // 点击按钮->注意触发方式不能使用trigger
+    primaryButton.vm.$emit('click')
   })
 })
