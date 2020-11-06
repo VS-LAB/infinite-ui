f5e223d785d808b83724c576a20117c89782e802
zhoubo <1522306890@qq.com>
2020-11-06 14:40:19 +0800

    feat(generate.js): 去掉了调式信息
    去掉了调式信息

--- examples/router/generate.js
+++ examples/router/generate.js
@@ -5,9 +5,6 @@ export function getFileList () {
   const blackList = ['./Installation.md']
   files.keys().map(key => {
     if (blackList.includes(key)) return
-    console.log('====================================')
-    console.log(key.replace(/\.\//g, '').replace(/.md/g, ''))
-    console.log('====================================')
     const name = key.replace(/\.\//g, '').replace(/.md/g, '')
     const element = {
       path: `/guide/${name}`,
@@ -17,7 +14,6 @@ export function getFileList () {
         type: 'component'
       }
     }
-    console.log(element)
     result.push(element)
   })
   return result
