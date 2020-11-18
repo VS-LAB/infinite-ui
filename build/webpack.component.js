b3ef3115640adc52c957e9f608177bd77c75108a
shmilyvidian <shmilyvidian@163.com>
2020-11-18 14:53:06 +0800

    feat: 忽略vue打包

--- build/webpack.component.js
+++ build/webpack.component.js
@@ -29,7 +29,12 @@ const webpackConfig = {
     // }
   },
   externals: {
-    vue: 'vue'
+    vue: {
+      root: 'Vue',
+      commonjs: 'vue',
+      commonjs2: 'vue',
+      amd: 'vue'
+    }
   },
   resolve: {
     extensions: ['.js', '.vue', '.json']
--- packages/index.js
+++ packages/index.js
@@ -1,40 +1,72 @@
 /* eslint-disable */
-const packageJson = require('../package.json')
-const version = process.env.VERSION || packageJson.version
-const files = require.context('./', true, /^(\.\/infinite(\-[a-z]+)+)\/index.js$/)
-function getImportComponents () {
-  const result = []
-  files.keys().map(key => {
-    if (!key.includes('infinite')) return
-    const element = files(key).default
-    result.push(element)
-  });
-  return result
-};
+// This file is auto gererated by build/build-entry.js
+    import InfiniteButton from './infinite-button'
+import InfiniteCascaders from './infinite-cascaders'
+import InfiniteDialog from './infinite-dialog'
+import InfiniteForm from './infinite-form'
+import InfiniteLineChart from './infinite-line-chart'
+import InfiniteNavMenu from './infinite-nav-menu'
+import InfinitePagination from './infinite-pagination'
+import InfinitePieChart from './infinite-pie-chart'
+import InfiniteSelectTags from './infinite-select-tags'
+import InfiniteSelectTagsOption from './infinite-select-tags-option'
+import InfiniteStep from './infinite-step'
+import InfiniteSteps from './infinite-steps'
+import InfiniteTable from './infinite-table'
+import InfiniteTableColumn from './infinite-table-column'
+import InfiniteThemePicker from './infinite-theme-picker'
+import InfiniteTree from './infinite-tree'
+    const version = '0.0.3'
+    const components = [
+      InfiniteButton,
+  InfiniteCascaders,
+  InfiniteDialog,
+  InfiniteForm,
+  InfiniteLineChart,
+  InfiniteNavMenu,
+  InfinitePagination,
+  InfinitePieChart,
+  InfiniteSelectTags,
+  InfiniteSelectTagsOption,
+  InfiniteStep,
+  InfiniteSteps,
+  InfiniteTable,
+  InfiniteTableColumn,
+  InfiniteThemePicker,
+  InfiniteTree
+    ]
+    const install = Vue => {
+      components.forEach(Component => {
+        Vue.use(Component)
+      })
 
-const components = getImportComponents()
-const install = Vue => {
-  components.forEach(Component => {
-    Vue.use(Component)
-  })
-};
-
-/* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-const exportObj = {
-  install,
-  version,
-}
-
-components.map(component => {
-  if (component) {
-    const { name } = component
-    if (!name) return
-    exportObj[name] = component
-  };
-});
-
-export default exportObj
+    };
+    /* istanbul ignore if */
+    if (typeof window !== 'undefined' && window.Vue) {
+      install(window.Vue)
+    }
+    export {
+      install,
+      version,
+      InfiniteButton,
+  InfiniteCascaders,
+  InfiniteDialog,
+  InfiniteForm,
+  InfiniteLineChart,
+  InfiniteNavMenu,
+  InfinitePagination,
+  InfinitePieChart,
+  InfiniteSelectTags,
+  InfiniteSelectTagsOption,
+  InfiniteStep,
+  InfiniteSteps,
+  InfiniteTable,
+  InfiniteTableColumn,
+  InfiniteThemePicker,
+  InfiniteTree
+    }
+    export default {
+      install,
+      version
+    }
+  
