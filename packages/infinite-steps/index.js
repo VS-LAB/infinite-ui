8571fb78d8c2356908e72e43c604c2efd7e76f8a
WeiZe <501816423@qq.com>
2020-11-26 10:24:15 +0800

    feat:移除step组件

--- packages/infinite-steps/index.js
+++
@@ -1,6 +0,0 @@
-import InfiniteSteps from './src/index'
-// 为组件提供 install 安装方法，供按需引入
-InfiniteSteps.install = function (Vue) {
-  Vue.component(InfiniteSteps.name, InfiniteSteps)
-}
-export default InfiniteSteps
--- packages/infinite-table/src/index.vue
+++ packages/infinite-table/src/index.vue
@@ -219,7 +219,7 @@ export default {
       })
     }
   },
-  destroyed () {
+  beforeDestroy () {
     this.computedHeightData = () => { }
   }
 }
