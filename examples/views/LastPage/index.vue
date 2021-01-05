9b7c2bfdcab30f00e7d439511c6f482e34c55370
zhiplus <925249766@qq.com>
2021-01-05 10:10:33 +0800

    fix:lastPage延迟执行setAutoFill, 才能正确设置scale缩放值

--- examples/views/LastPage/index.vue
+++ examples/views/LastPage/index.vue
@@ -145,7 +145,7 @@ export default {
       const { height: explainFooterHeight, bottom } = explainFooter.getBoundingClientRect()
       const explainFooterBottom = clientHeight - bottom
       const thresholdValue = 0.1 // 防止不够，多缩小一点
-      console.log('(clientHeight - height - explainFooterHeight)', (clientHeight - height - explainFooterHeight), (clientHeight - height - explainFooterHeight) < 100);
+      console.log('(clientHeight - height - explainFooterHeight)', (clientHeight - height - explainFooterHeight), (clientHeight - height - explainFooterHeight) < 100)
       if ((clientHeight - height - explainFooterHeight) < 100) {
         // const scale = 1 - Math.abs(clientHeight - height - explainFooterHeight) / height - thresholdValue
         // //  如果此时任然不够
@@ -196,7 +196,9 @@ export default {
   },
   mounted () {
     document.body.style.overflow = 'auto'
-    this.setAutoFill()
+    setTimeout(() => {
+      this.setAutoFill()
+    }, 100)
   }
 }
 </script>
